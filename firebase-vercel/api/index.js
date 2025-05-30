4fc5609bf06b94f1599fff4e3bb24ea4829ee3f5
Damian Florin Dragos <ddragos-external@soldo.com>
2025-05-30 20:36:44 +0200

    feat: add default endpoint with description

---
+++ firebase-vercel/api/index.js
@@ -0,0 +1,5 @@
+module.exports = async (req, res) => {
+  res.send(
+    "Firebase + Vercel API starter, see repo at https://github.com/RayRemnant/starters-monorepo/tree/main/firebase-vercel"
+  )
+}
