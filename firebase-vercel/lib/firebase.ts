1be06846ec12b743ddebdea3b22d948cd891fa91
Damian Florin Dragos <ddragos-external@soldo.com>
2025-06-01 00:15:58 +0200

    bugfix: change env names to match github actions secrets

--- firebase-vercel/lib/firebase.ts
+++ firebase-vercel/lib/firebase.ts
@@ -12,12 +12,12 @@ interface ActualDocumentData extends DocumentData {
 }
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  appId: process.env.appId,
-  authDomain: process.env.authDomain,
-  messagingSenderId: process.env.messagingSenderId,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket
+  apiKey: process.env.FIREBASE_API_KEY,
+  appId: process.env.FIREBASE_APP_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 }
 
 const app = initializeApp(firebaseConfig)
