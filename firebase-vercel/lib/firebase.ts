830e1814e9edc7e6f1c12a6face25cfb2abf9f92
Damian Florin Dragos <ddragos-external@soldo.com>
2025-05-31 20:51:36 +0200

    feat: add more accurate interface to document type

--- firebase-vercel/lib/firebase.ts
+++ firebase-vercel/lib/firebase.ts
@@ -1,5 +1,15 @@
+import type { DocumentData } from "firebase/firestore/lite"
+
 import { initializeApp } from "firebase/app"
 import * as firestore from "firebase/firestore/lite"
+
+interface ActualDocumentData extends DocumentData {
+  id: string
+  updated: {
+    nanoseconds: number
+    seconds: number
+  }
+}
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
@@ -14,21 +24,29 @@ const app = initializeApp(firebaseConfig)
 const db = firestore.getFirestore(app)
 
 // Get single doc
-async function getDoc<T = unknown>(collectionName: string, docId: string): Promise<{} | T> {
+async function getDoc<T extends ActualDocumentData = ActualDocumentData>(
+  collectionName: string,
+  docId: string
+): Promise<T | undefined> {
   const docRef = firestore.doc(db, collectionName, docId)
   const docSnap = await firestore.getDoc(docRef)
-  return docSnap.exists() ? (docSnap.data() as T) : {}
+  return docSnap.data() as T | undefined
 }
 
 // Get multiple docs
-async function getDocs<T = unknown>(collectionName: string): Promise<T[]> {
+async function getDocs<T extends ActualDocumentData = ActualDocumentData>(
+  collectionName: string
+): Promise<T[]> {
   const collectionData = firestore.collection(db, collectionName)
   const collectionSnapshot = await firestore.getDocs(collectionData)
   return collectionSnapshot.docs.map(_doc => _doc.data() as T)
 }
 
 // Set single doc
-async function setDoc<T extends { id: string }>(collectionName: string, doc: T): Promise<void> {
+async function setDoc<T extends Pick<ActualDocumentData, "id"> = Pick<ActualDocumentData, "id">>(
+  collectionName: string,
+  doc: T
+): Promise<void> {
   await firestore.setDoc(firestore.doc(db, collectionName, doc.id), {
     ...doc,
     updated: firestore.serverTimestamp()
@@ -36,7 +54,10 @@ async function setDoc<T extends { id: string }>(collectionName: string, doc: T):
 }
 
 // Set multiple docs
-async function setDocs<T extends { id: string }>(collectionName: string, docs: T[]): Promise<void> {
+async function setDocs<T extends Pick<ActualDocumentData, "id"> = Pick<ActualDocumentData, "id">>(
+  collectionName: string,
+  docs: T[]
+): Promise<void> {
   for (const doc of docs) {
     await setDoc(collectionName, doc)
   }
